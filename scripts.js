2a5dee4425f68f9d6795dae66f1956e451a54bb8
Glaucco <glauccolima@gmail.com>
2023-01-29 17:35:33 -0300

    Update

---
+++ scripts.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict'
+
+    var forms = document.querySelectorAll('.needs-validation');
+
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+
+            form.addEventListener('submit', function (event) {
+
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add('was-validated');
+
+            }, false);
+
+        });
+
+})()
